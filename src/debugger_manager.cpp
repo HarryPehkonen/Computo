f024fa06751d4593d74a65e3231232b14d3ebe2c
Harri Pehkonen <harry.pehkonen@gmail.com>
2025-07-02 14:51:33 -0700

    thread-safety:  debug manager was not protected.  fixed!

--- src/debugger_manager.cpp
+++ src/debugger_manager.cpp
@@ -4,8 +4,8 @@
 
 namespace computo {
 
-// Global debugger instance
-static std::unique_ptr<Debugger> global_debugger = nullptr;
+// Thread-local debugger instance - each thread has its own debugger
+thread_local std::unique_ptr<Debugger> global_debugger = nullptr;
 
 void set_debugger(std::unique_ptr<Debugger> debugger) {
     global_debugger = std::move(debugger);
